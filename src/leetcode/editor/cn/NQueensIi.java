5f21616d34683e4ace38c3d826336414d19cbc30
huajun.wu <753415348@qq.com>
2021-10-12 07:29:28 +0800

    Initial Commit

---
+++ src/leetcode/editor/cn/NQueensIi.java
@@ -0,0 +1,49 @@
+//<p><strong>n 皇后问题</strong> 研究的是如何将 <code>n</code> 个皇后放置在 <code>n×n</code> 的棋盘上，并且使皇后彼此之间不能相互攻击。</p>
+//
+//<p>给你一个整数 <code>n</code> ，返回 <strong>n 皇后问题</strong> 不同的解决方案的数量。</p>
+//
+//<p> </p>
+//
+//<div class="original__bRMd">
+//<div>
+//<p><strong>示例 1：</strong></p>
+//<img alt="" src="https://assets.leetcode.com/uploads/2020/11/13/queens.jpg" style="width: 600px; height: 268px;" />
+//<pre>
+//<strong>输入：</strong>n = 4
+//<strong>输出：</strong>2
+//<strong>解释：</strong>如上图所示，4 皇后问题存在两个不同的解法。
+//</pre>
+//
+//<p><strong>示例 2：</strong></p>
+//
+//<pre>
+//<strong>输入：</strong>n = 1
+//<strong>输出：</strong>1
+//</pre>
+//
+//<p> </p>
+//
+//<p><strong>提示：</strong></p>
+//
+//<ul>
+//	<li><code>1 <= n <= 9</code></li>
+//	<li>皇后彼此不能相互攻击，也就是说：任何两个皇后都不能处于同一条横行、纵行或斜线上。</li>
+//</ul>
+//</div>
+//</div>
+//<div><div>Related Topics</div><div><li>回溯</li></div></div><br><div><li>👍 299</li><li>👎 0</li></div>
+  
+package leetcode.editor.cn;
+public class NQueensIi{
+  public static void main(String[] args) {
+       Solution solution = new NQueensIi().new Solution();
+  }
+  //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int totalNQueens(int n) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
